fb8546b7d642298f2c5714944b881920be0b6df3
Arvid Sahlin <arvid@pario.se>
2019-01-04 10:07:04 +0100

    new text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Home of pario.
+            Home of Pario
           </p>
           <a
             className="App-link"
